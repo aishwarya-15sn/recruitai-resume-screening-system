006e22337d6beeae5d03c5ccf677a97de1754737
Aishwarya Shanmukh <aishwarya153009@gmail.com>
2026-05-08 10:03:02 +0530

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,1 +1,11 @@
+def extract_skills(text):
+    skills_db = ["python", "sql", "machine learning", "fastapi", "nlp"]
+    found = []
 
+    text = text.lower()
+
+    for skill in skills_db:
+        if skill in text:
+            found.append(skill)
+
+    return found
