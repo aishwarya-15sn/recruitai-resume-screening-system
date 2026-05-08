42ab104efaa8f214f49064268cdb26eafee90f35
Aishwarya Shanmukh <aishwarya153009@gmail.com>
2026-05-08 10:04:49 +0530

    Update ranking_model.py

--- ranking_model.py
+++ ranking_model.py
@@ -1,1 +1,21 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+job_description = """
+python sql machine learning nlp fastapi
+"""
+
+def rank_candidate(skills):
+    resume_text = " ".join(skills)
+
+    documents = [resume_text, job_description]
+
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(documents)
+
+    similarity = cosine_similarity(
+        tfidf_matrix[0:1],
+        tfidf_matrix[1:2]
+    )
+
+    return round(similarity[0][0] * 100, 2)
